1d1eeb1db97fdcac34f0d64901f7d139cde8bd78
srinumocharla9 <110721518+srinumocharla9@users.noreply.github.com>
2022-12-27 21:06:46 +0530

    Update todo.js

--- models/todo.js
+++ models/todo.js
@@ -15,6 +15,10 @@ module.exports = (sequelize, DataTypes) => {
       return this.create({ title: title, dueDate: dueDate, completed: false });
     }
 
+    static getTodos() {
+      return this.findAll({ order: [["id", "ASC"]] });
+    }
+
     markAsCompleted() {
       return this.update({ completed: true });
     }
