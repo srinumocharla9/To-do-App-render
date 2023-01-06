1c578775c0a79fe2492605c8253e822d04bbfa5f
srinumocharla9 <110721518+srinumocharla9@users.noreply.github.com>
2023-01-06 21:24:45 +0530

    Update todo.js

--- models/todo.js
+++ models/todo.js
@@ -1,5 +1,6 @@
 "use strict";
-const { Model } = require("sequelize");
+const { Model, Op } = require("sequelize");
+
 module.exports = (sequelize, DataTypes) => {
   class Todo extends Model {
     /**
@@ -10,17 +11,66 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
     }
-    static addTodo({ title, dueDate }) {
+    static addaTodo({ title, dueDate }) {
       return this.create({ title: title, dueDate: dueDate, completed: false });
     }
-
-    markAsCompleted() {
-      return this.update({ completed: true });
+    // static markAsCompleted() {
+    //   return this.update({ completed: true });
+    // }
+    static getAllTodos() {
+      return this.findAll({ order: [["id", "ASC"]] });
     }
-    static getTodos() {
-      return this.findAll();
+    static async completedItemsAre() {
+      return this.findAll({
+        where: { completed: { [Op.eq]: true } },
+        order: [["id", "DESC"]],
+      });
+    }
+    static async remove(id) {
+      return this.destroy({
+        where: {
+          id,
+        },
+      });
+    }
+    setCompletionStatusAs(bool) {
+      return this.update({ completed: bool });
+    }
+    static async overdue() {
+      return this.findAll({
+        where: {
+          dueDate: {
+            [Op.lt]: new Date().toLocaleDateString("en-CA"),
+          },
+          completed: false,
+        },
+        order: [["id", "ASC"]],
+      });
+    }
+    static async dueToday() {
+      return this.findAll({
+        where: {
+          dueDate: {
+            [Op.eq]: new Date().toLocaleDateString("en-CA"),
+          },
+          completed: false,
+        },
+        order: [["id", "ASC"]],
+      });
+    }
+    static async dueLater() {
+      return this.findAll({
+        where: {
+          dueDate: {
+            [Op.gt]: new Date().toLocaleDateString("en-CA"),
+          },
+          completed: false,
+        },
+        order: [["id", "ASC"]],
+      });
     }
   }
+
   Todo.init(
     {
       title: DataTypes.STRING,
