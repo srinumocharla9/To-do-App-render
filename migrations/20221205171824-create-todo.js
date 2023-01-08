d188020746de9a3dde0fd6a9ba2de4e1b0f699d6
srinumocharla9 <110721518+srinumocharla9@users.noreply.github.com>
2023-01-09 00:41:43 +0530

    Create 20221205171824-create-todo.js

---
+++ migrations/20221205171824-create-todo.js
@@ -0,0 +1,36 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Todos", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      dueDate: {
+        type: Sequelize.DATEONLY,
+      },
+      completed: {
+        type: Sequelize.BOOLEAN,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Todos");
+  },
+};
